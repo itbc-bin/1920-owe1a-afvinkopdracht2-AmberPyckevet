0a3cb114db7d2dcb99e6cbb441bf72516cc90883
AmberPyckevet <55191432+AmberPyckevet@users.noreply.github.com>
2019-09-14 03:45:43 +0200

    Update weektaak 2 2019.py

--- weektaak 2 2019.py
+++ weektaak 2 2019.py
@@ -1,15 +1,14 @@
-file = 'C:\\Users\\Amber.DESKTOP-MANUOL0\\Documents\\School\\han\\genes\\test_dna.txt'
+file = 'insert\\full\\file\\he_re'
 f = open(file, "r")
 lines = f.readlines()
 skipline = True
 for line in lines:
     if skipline == False:
-        aantal_a = line.count("a")
-        aantal_t = line.count("t")
-        aantal_c = line.count("c")
-        aantal_g = line.count("g")
+        aantal_a = line.count("A")
+        aantal_t = line.count("T")
+        aantal_c = line.count("C")
+        aantal_g = line.count("G")
         lengte = len(line)
         cg=((aantal_c+aantal_g)/lengte*100)
         print(str(cg)+"%")
     skipline = False
-
